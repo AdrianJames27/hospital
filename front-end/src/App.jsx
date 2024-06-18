8c08255016fa11e761d0a20965c237fa178bb30c
AdrianJames27 <161918152+AdrianJames27@users.noreply.github.com>
2024-06-18 18:51:42 +0800

    update

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,160 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserLogin from './component/UserLogin';
+import StaffPanel from './component/StaffPanel';
+import PatientPanel from './component/PatientPanel';
+import PatientRegistration from './component/PatientRegistration';
+import StaffRegistration from './component/StaffRegistration';
+import ManageUsers from './component/ManageUsers';
+import ManagePatientRecords from './component/ManagePatientRecords';
+import PatientMedicalRecords from './component/PatientMedicalRecords';
+import ManageDoctors from './component/ManageDoctors';
+import ViewAppoinments from './component/ViewAppointments';
+import ViewMedicalRecords from './component/ViewMedicalRecords';
+import BookAppointment from './component/BookAppointment';
+import ManageAppointments from './component/ManageAppointments';
+import ManageMedicalRecords from './component/ManageMedicalRecords';
+import DoctorViewProfile from './component/DoctorViewProfile';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <HashRouter>
+        <Routes>
+          {
+            // login route
+          }
+
+          <Route
+            exact
+            path='/'
+            element={<UserLogin />}
+          />
+
+          {
+            // admin routes
+          }
+
+          <Route
+            path='/hospital/admin/panel'
+            element={<StaffPanel />}
+          />
+
+          <Route
+            path='/hospital/admin/manage_user'
+            element={<ManageUsers />}
+          />
+
+          <Route
+            path='/hospital/admin/manage_patient_records'
+            element={<ManagePatientRecords />}
+          />
+
+          <Route
+            path='/hospital/admin/manage_doctors'
+            element={<ManageDoctors />}
+          />
+
+          <Route
+            path='/hospital/admin/view_appointments'
+            element={<ViewAppoinments />}
+          />
+
+          <Route
+            path='/hospital/admin/view_medical_records'
+            element={<ViewMedicalRecords />}
+          />
+
+          {
+            // patient routes
+          }
+
+          <Route
+            path='/hospital/patient/panel'
+            element={<PatientPanel />}
+          />
+
+          <Route
+            path='/hospital/patient/book_appointment'
+            element={<BookAppointment />}
+          />
+
+          <Route 
+            path='/hospital/patient/manage_appointments'
+            element={<ManageAppointments />}
+          />
+
+          <Route
+            path='/hospital/patient/view_medical_records'
+            element={<PatientMedicalRecords />}
+          />
+
+          {
+            // doctor routes
+          }
+
+          <Route
+            path='/hospital/doctor/panel'
+            element={<StaffPanel />}
+          />
+
+          <Route
+            path='/hospital/doctor/profile'
+            element={<DoctorViewProfile />}
+          />
+
+          <Route
+            path='/hospital/doctor/manage_patient_records'
+            element={<ManagePatientRecords />}
+          />
+
+          <Route
+            path='/hospital/doctor/manage_medical_records'
+            element={<ManageMedicalRecords />}
+          />
+
+          <Route
+            path='/hospital/doctor/manage_appointments'
+            element={<ManageAppointments />}
+          />
+
+          {
+            // receptionist routes
+          }
+
+          <Route
+            path='/hospital/receptionist/panel'
+            element={<StaffPanel />}
+          />
+
+          <Route
+            path='/hospital/receptionist/manage_patient_records'
+            element={<ManagePatientRecords />}
+          />
+
+          <Route
+            path='/hospital/receptionist/book_appointment'
+            element={<BookAppointment />}
+          />
+
+          {
+            // registration routes
+          }
+
+          <Route
+            path='/hospital/user/patient/register'
+            element={<PatientRegistration />}
+          />
+
+          <Route
+            path='/hospital/user/staff/register'
+            element={<StaffRegistration />}
+          />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ front-end/src/component/PatientPanel.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import PatientNavigation from './PatientNavigation';
+
+export default function PatientPanel() {
+    const navigate = useNavigate();
+    let userSession;
+
+    useEffect(() => {
+        userSession = JSON.parse(sessionStorage.getItem('userSession'));
+
+        // if userSession is empty, go back to login page
+        if (!userSession) {
+            navigate('/');
+        } else {
+            // if role is not patient, go back to login page
+            if (userSession.role !== 'patient') navigate('/');
+        }
+    }, []);
+
+    return (
+        <div class="dashboard d-flex justify-content-center container-fluid vh-100">
+            <PatientNavigation />
+            <div>
+                <h1 class="welc container-fluid">Welcome {userSession.name}!</h1>
+            </div>
+        </div>
+    );
+}
